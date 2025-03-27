ca0435940e51fdbcd87deef30980d6a0daf24505
Your Name <you@example.com>
2025-03-27 23:54:31 +0600

    remove

--- assianment-four.js
+++ assianment-four.js
@@ -1,73 +1,58 @@
-// task-1 calculator
 function cashOut(money) {
-    if (typeof money === 'number') {
-        if (money < 0) {
-            return "Invalid"
-        }
-        const result = money * 1.75 / 100
-        return result
-    }
-    else {
+    if (money < 0 || typeof money != 'number') {
         return "Invalid"
     }
+    let result = money * 1.75 / 100
+    return result
 }
-// task-2
 function validEmail(email) {
     if (typeof email === 'string') {
-        const charecter='-._+@'
+        const charecter = '-._+@'
         if (charecter.includes(email[0])) {
             return false
         }
-        else if (!email.endsWith('.com')&& !email.includes('@')) {
+        else if (!email.endsWith('.com') || !email.includes('@')) {
             return false
         }
-        else if(email.includes(' ')){
+        else if (email.includes(' ')) {
             return false
 
-        }
-        else{
-            return true
-        }
-    }
-    else{
-        return "Invalid"
-    }
-}
-
-// task3
-function electionResult(votes) {
-    if (Array.isArray(votes) === true) {
-        let mango = 0;
-        let banana = 0;
-        for (vote of votes) {
-            if (vote === 'mango') {
-                mango++;
-
-            }
-            else if (vote === 'banana') {
-                banana++;
-            }
-
-        }
-        if (mango > banana) {
-            return "Mango"
-
-        }
-        else if (banana > mango) {
-            return "Banana"
         }
         else {
-            return "Draw"
+            return true
         }
-
-
-
     }
     else {
         return "Invalid"
     }
 }
-// task-4
+function electionResult(votes) {
+    if (Array.isArray(votes) != true) {
+        return "Invalid"
+    }
+    let mango = 0;
+    let banana = 0;
+    for (vote of votes) {
+        if (vote === 'mango') {
+            mango++;
+
+        }
+        else if (vote === 'banana') {
+            banana++;
+        }
+
+    }
+    if (mango > banana) {
+        return "Mango"
+
+    }
+    else if (banana > mango) {
+        return "Banana"
+    }
+
+    return "Draw"
+
+}
 function isBestFriend(f1, f2) {
     if (typeof f1 === 'object' && typeof f2 === 'object' && !Array.isArray(f1) && !Array.isArray(f2)) {
         if (f1.roll === f2.bestFriend && f2.roll === f1.bestFriend) {
@@ -83,46 +68,23 @@ function isBestFriend(f1, f2) {
         return 'Invalid'
     }
 }
-// task 5
 function calculateWatchTime(times) {
-    if (Array.isArray(times) === true) {
-        const lastResult = {
-            hour: 0,
-            minute: 0,
-            second: 0
-        }
-        let totalTimeSec = 0
-        for (const time of times) {
-            if (typeof time === 'number' && time > 0) {
-                totalTimeSec += time
-            }
-            else {
-                return "Invalid"
-            }
-
-
-        }
-
-        const hour = parseInt(totalTimeSec / 3600)
-        lastResult.hour = hour;
-        const remainingMinute = totalTimeSec % 3600
-        const minute = parseInt(remainingMinute / 60)
-        lastResult.minute = minute
-        const second = remainingMinute % 60
-        lastResult.second = second
-
-        return lastResult
-
-    }
-    else {
+    if (Array.isArray(times) != true) {
         return "Invalid"
     }
+    let totalTimeSec = 0
+    for (const time of times) {
+        if (typeof time === 'number' && time >= 0) {
+            totalTimeSec += time
+        }
+        else {
+            return "Invalid"
+        }
+    }
+    const hour = parseInt(totalTimeSec / 3600)
+    const remainingMinute = totalTimeSec % 3600
+    const minute = parseInt(remainingMinute / 60)
+    const second = remainingMinute % 60
+    return {hour,minute,second}
+
 }
-
-
-
-
-
-
-
-
